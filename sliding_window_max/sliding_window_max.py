db6572001e749b00defcaae8b87d94accebaf450
Nate Dow <ndow33@gmail.com>
2020-09-16 13:07:44 -0600

    sliding window

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -3,9 +3,27 @@ Input: a List of integers as well as an integer `k` representing the size of the
 Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
-    # Your code here
+    # Create an array to hold the max values
+    maxes = []
 
-    pass
+    # length = Find the length of the input array
+    length = len(nums)
+    # instantiate a beginning index of the window (example: i = 0)
+    i = 0
+    # while index k is inside of the array
+    while k <= length:
+        # Create a sub array from index i (inclusive) to index k (exclusive)
+        sub = nums[i:k]
+        # find the max value in the sub array
+        # append it to the maxes array
+        maxes.append(max(sub))
+        # increment k += 1
+        k += 1
+        # increment i += 1
+        i += 1
+
+    # return the output
+    return maxes
 
 
 if __name__ == '__main__':
