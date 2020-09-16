f9b346e10ca8500559bc9d47473cb59b8b063b45
Nate Dow <ndow33@gmail.com>
2020-09-16 16:32:31 -0600

    cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,35 @@
 Input: an integer
 Returns: an integer
 '''
+# how many ways can cookie monster eat n cookies?
 def eating_cookies(n):
-    # Your code here
+    # create an array with the base values
+    base = [1, 1, 2]
+    # if n is an index in the base array:
+    if n < 3:
+        # return the value at that index
+        return base[n]
+    # if n is outside of the index
+    elif n >= 3:
+        # instantiate a value that represents our current index
+        index = 2
+        # as long as the length of the array is less than n
+        while index < n:
+            # we need to add the values within the array to each other
+            # and append that to the end of the array
+            base.append(sum(base))
+            # remove the first value of the array
+            base.pop(0)
+            # increment index by 1
+            index += 1
+    # once we have gotten to the nth iteration, return the 2nd index value in base      
+    return base[2]
 
-    pass
+
+    
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 5
+    num_cookies = 4
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to eat {num_cookies} cookies")
